48cc1cb312885f58e12d4ec03d832d054dd71241
Nazarii Lesniak <naziksay@gmail.com>
2026-02-10 15:57:27 +0200

    changed api

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=1a67f070';
+const API_KEY = '1a67f070';
+
+const API_URL = `https://www.omdbapi.com/?apikey=${encodeURIComponent(API_KEY)}`;
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)
